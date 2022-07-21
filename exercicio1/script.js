55d10f11bff481d8a337f13b7715e16e80656eb3
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-21 14:12:06 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let pergunta = prompt("Digite um número");
+let par = pergunta%2;
+console.log(par);
+console.log(typeof par);
+
+/*o valor sempre retorna a 0 pois não existe resto da divisão de números
+pares por pares*/
+/*quando o úsuario coloca um número impor a resposta ira variar de acordo
+com a sobra da divisão de número impar por 2, exemplo 5 % 2 = 1*/
