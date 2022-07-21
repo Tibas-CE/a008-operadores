d1c1a8fee729aa34a1365068309f760c0cc1a050
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-21 14:13:27 -0300

    feito

---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+const operacao = 5 > 20 && 5 < 2;
+console.log(operacao);
+const operacao1 = 5 === 5 || "5";
+console.log(operacao1);
+const operacao2 = !"vinte" > "cinquenta";
+console.log(operacao2);
+const operacao3 = !("vinte" > "cinquenta");
+console.log(operacao3);
